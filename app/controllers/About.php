da7297937930d19600f3adbb37c0baebd937da5b
deyos65 <dedeyosep@mail.unpas.ac.id>
2019-10-07 06:27:46 -0700

    View MVC

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,22 @@
 <?php 
 
-class About {
-	public function index($nama = 'Shandika', $pekerjaan = 'Dosen')
+class About extends Controller {
+	public function index($nama = 'Shandika', $pekerjaan = 'Dosen', $umur = 32)
 	{
-		echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+		$data['nama'] = $nama;
+		$data['pekerjaan'] = $pekerjaan;
+		$data['umur'] = $umur;
+		$data['judul'] = 'About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
+
 	public function page()
 	{
-		echo 'About/page';
+		$data['judul'] = 'Pages';
+		$this->view('templates/header', $data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
